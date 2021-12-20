172b1e3ee802d45c6b09a5025635701ea05f351c
guya ka <1722967878@qq.com>
2021-12-20 16:16:28 +0800

    2

---
+++ SDMS/FrmControl.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace SDMS
+{
+    partial class FrmControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvInfo = new System.Windows.Forms.DataGridView();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvInfo
+            // 
+            this.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInfo.Location = new System.Drawing.Point(12, 12);
+            this.dgvInfo.Name = "dgvInfo";
+            this.dgvInfo.RowTemplate.Height = 23;
+            this.dgvInfo.Size = new System.Drawing.Size(776, 320);
+            this.dgvInfo.TabIndex = 0;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(115, 386);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 1;
+            this.btnNew.Text = "新增";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(319, 395);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 1;
+            this.btnChange.Text = "修改";
+            this.btnChange.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(621, 395);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "删除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // FrmControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.dgvInfo);
+            this.Name = "FrmControl";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvInfo;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
--- SDMS/FrmControl.cs
+++ SDMS/FrmControl.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SDMS
 {
-    public partial class Form1 : Form
+    public partial class FrmControl : Form
     {
-        public Form1()
+        public FrmControl()
         {
             InitializeComponent();
         }
